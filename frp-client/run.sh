8d5629d169c643cc98c35330ab6972e42acbb775
Joey <28235447+LonelystartO@users.noreply.github.com>
2025-08-06 20:48:45 +0800

    Update run.sh

--- frp-client/run.sh
+++ frp-client/run.sh
@@ -10,7 +10,7 @@ function stop_frpc() {
 
 bashio::log.info "Copying configuration."
 cp $DEFAULT_CONFIG_PATH $CONFIG_PATH
-sed -i "s/serverAddr = \"your_server_addr\"/serverAddr = \"$(bashio::config 'serverAddr')\"/" $CONFIG_PATH
+sed -i "s/serverAddr = \"your_server_addr\"/serverAddr = \"$(bashio::config \"serverAddr\")\"/" $CONFIG_PATH
 sed -i "s/serverPort = 7000/serverPort = $(bashio::config 'serverPort')/" $CONFIG_PATH
 sed -i "s/auth.token = \"123456789\"/auth.token = \"$(bashio::config 'authToken')\"/" $CONFIG_PATH
 #sed -i "s/webServer.port = 7500/webServer.port = $(bashio::config 'webServerPort')/" $CONFIG_PATH
