d81e39ea4104aa99b9c94e43f7ef3a59f518cf6c
Joey <28235447+LonelystartO@users.noreply.github.com>
2025-08-06 20:43:42 +0800

    Update run.sh

--- frp-client/run.sh
+++ frp-client/run.sh
@@ -10,14 +10,14 @@ function stop_frpc() {
 
 bashio::log.info "Copying configuration."
 cp $DEFAULT_CONFIG_PATH $CONFIG_PATH
-#sed -i "s/serverAddr = \"your_server_addr\"/serverAddr = \"$(bashio::config 'serverAddr')\"/" $CONFIG_PATH
-#sed -i "s/serverPort = 7000/serverPort = $(bashio::config 'serverPort')/" $CONFIG_PATH
-#sed -i "s/auth.token = \"123456789\"/auth.token = \"$(bashio::config 'authToken')\"/" $CONFIG_PATH
+sed -i "s/serverAddr = \"your_server_addr\"/serverAddr = \"$(bashio::config 'serverAddr')\"/" $CONFIG_PATH
+sed -i "s/serverPort = 7000/serverPort = $(bashio::config 'serverPort')/" $CONFIG_PATH
+sed -i "s/auth.token = \"123456789\"/auth.token = \"$(bashio::config 'authToken')\"/" $CONFIG_PATH
 #sed -i "s/webServer.port = 7500/webServer.port = $(bashio::config 'webServerPort')/" $CONFIG_PATH
 #sed -i "s/webServer.user = \"admin\"/webServer.user = \"$(bashio::config 'webServerUser')\"/" $CONFIG_PATH
 #sed -i "s/webServer.password = \"123456789\"/webServer.password = \"$(bashio::config 'webServerPassword')\"/" $CONFIG_PATH
-#sed -i "s/customDomains = \[\"your_domain\"\]/customDomains = [\"$(bashio::config 'customDomain')\"]/" $CONFIG_PATH
-#sed -i "s/name = \"your_proxy_name\"/name = \"$(bashio::config 'proxyName')\"/" $CONFIG_PATH
+sed -i "s/customDomains = \[\"your_domain\"\]/customDomains = [\"$(bashio::config 'customDomain')\"]/" $CONFIG_PATH
+sed -i "s/name = \"your_proxy_name\"/name = \"$(bashio::config 'proxyName')\"/" $CONFIG_PATH
 
 tail -f /dev/null
 
@@ -27,10 +27,10 @@ bashio::log.info "Starting frp client"
 
 cat $CONFIG_PATH
 
-#cd /usr/src
-#./frpc -c $CONFIG_PATH & WAIT_PIDS+=($!)
+cd /usr/src
+./frpc -c $CONFIG_PATH & WAIT_PIDS+=($!)
 
-#tail -f /share/frpc.log &
+tail -f /share/frpc.log &
 
-#trap "stop_frpc" SIGTERM SIGHUP
-#wait "${WAIT_PIDS[@]}"
+trap "stop_frpc" SIGTERM SIGHUP
+wait "${WAIT_PIDS[@]}"
